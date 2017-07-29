fde725caf0d3549e6dbcfb630f2bcbf3c22d2f58
Stephanos Theodotou <stephanostheodotou@Stephanoss-MacBook-Pro.local>
2017-07-29 15:41:48 +0200

    add uppercase pipe

--- src/app/components/uppercase/uppercase.component.ts
+++ src/app/components/uppercase/uppercase.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./uppercase.component.css']
 })
 export class UppercaseComponent implements OnInit {
+name : string = "Andrew";
 
   constructor() { }
 
