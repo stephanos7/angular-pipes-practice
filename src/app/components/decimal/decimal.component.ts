6fade7ba413751a27965f1fbe47010e8b43f8990
Stephanos Theodotou <stephanostheodotou@Stephanoss-MacBook-Pro.local>
2017-07-29 17:22:56 +0200

    add default decimal pipe

--- src/app/components/decimal/decimal.component.ts
+++ src/app/components/decimal/decimal.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./decimal.component.css']
 })
 export class DecimalComponent implements OnInit {
-
+number: Number = 431.896554;
   constructor() { }
 
   ngOnInit() {
