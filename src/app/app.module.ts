cf80b0c8094e7879a606585e5c9ee284498ca659
Stephanos Theodotou <stephanostheodotou@Stephanoss-MacBook-Pro.local>
2017-07-29 16:57:57 +0200

    generate currency, decimal and slice components within second row

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,19 @@ import { AppComponent } from './app.component';
 import { DateComponent } from './components/date/date.component';
 import { UppercaseComponent } from './components/uppercase/uppercase.component';
 import { FilterComponent } from './components/filter/filter.component';
+import { CurrencyComponent } from './components/currency/currency.component';
+import { DecimalComponent } from './components/decimal/decimal.component';
+import { SliceComponent } from './components/slice/slice.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DateComponent,
     UppercaseComponent,
-    FilterComponent
+    FilterComponent,
+    CurrencyComponent,
+    DecimalComponent,
+    SliceComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/decimal/decimal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-decimal',
+  templateUrl: './decimal.component.html',
+  styleUrls: ['./decimal.component.css']
+})
+export class DecimalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
