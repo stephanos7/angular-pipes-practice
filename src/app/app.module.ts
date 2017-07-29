eb608d1688a12c30e897d7c117d4933f5f9bd022
Stephanos Theodotou <stephanostheodotou@Stephanoss-MacBook-Pro.local>
2017-07-29 15:19:49 +0200

    add initial component layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { DateComponent } from './components/date/date.component';
+import { UppercaseComponent } from './components/uppercase/uppercase.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DateComponent
+    DateComponent,
+    UppercaseComponent
   ],
   imports: [
     BrowserModule
